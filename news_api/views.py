c77a1696576f5d8c6debf3b41778728ae26a9879
dvarkesh <dvarkeshg9926@gmail.com>
2022-10-05 00:42:05 +0530

    modified:   news_api/__pycache__/views.cpython-310.pyc modified:   news_api/views.py

--- news_api/views.py
+++ news_api/views.py
@@ -1,37 +1,37 @@
+from os import access
+from tabnanny import check
 from django.shortcuts import render
 import requests
 API_KEY_NEWS = 'd0b69496c18e463f888a273cb521ea9f'
 API_KEY_WEATHER='aaba2d7cd73f453ca14144033222906'
-CITY='TIRUCHIRAPPALLI'
 CAT = 'Technology'
-CAN = 'in'
-
 def home(request):
     country = request.GET.get('country')
     category = request.GET.get('category')
-
+    url_for_location='http://ipinfo.io/14.139.162.2 ? token=0680a74dacbc7b'
+    response_of_location = requests.get(url_for_location)
+    data_of_location = response_of_location.json()
+    CITY = data_of_location['city']
+    CAN = data_of_location['country']
+    url_for_weather=f'http://api.weatherapi.com/v1/current.json?key={API_KEY_WEATHER} &q={CITY}&aqi=no'
+    response_of_weather = requests.get(url_for_weather)
+    data_of_weather = response_of_weather.json()
     if category:
-        url1=f'http://api.weatherapi.com/v1/current.json?key={API_KEY_WEATHER} &q={CITY}&aqi=no'
-        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY_NEWS}&country={CAN}'
-        response = requests.get(url)
-        response1 = requests.get(url1)
-        data = response.json()
-        data1 = response1.json()
-        articles = data['articles']
+        url_for_news = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={API_KEY_NEWS}&country={CAN}'
+        response_of_news = requests.get(url_for_news)    
+        data_of_news = response_of_news.json()
+        articles = data_of_news['articles']
     else:
-        url1=f'http://api.weatherapi.com/v1/current.json?key={API_KEY_WEATHER} &q={CITY}&aqi=no'
-        url = f'https://newsapi.org/v2/top-headlines?category={CAT}&apiKey={API_KEY_NEWS}&country={CAN}'
-        response = requests.get(url)
-        response1 = requests.get(url1)
-        data = response.json()
-        data1 = response1.json()
-        articles = data['articles']
+        url_for_news = f'https://newsapi.org/v2/top-headlines?category={CAT}&apiKey={API_KEY_NEWS}&country={CAN}'
+        response_of_news = requests.get(url_for_news)
+        data_of_news = response_of_news.json()
+        articles = data_of_news['articles']
 
     weather = {
-        'Name' :data1['location']['name'],
-        'temp' :data1['current']['temp_c'],
-        'description' : data1['current']['condition']['text'],
-        'icon' :data1['current']['condition']['icon'],
+        'Name' :data_of_weather['location']['name'],
+        'temp' :data_of_weather['current']['temp_c'],
+        'description' : data_of_weather['current']['condition']['text'],
+        'icon' :data_of_weather['current']['condition']['icon'],
     }
     context={
         'weather' : weather,
